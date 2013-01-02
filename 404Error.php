64638e925e2eaec13ef3b5060b1f936b5d7b3b4c
Mapk <uraine@gmail.com>
2013-01-02 10:06:49 -0800

    Added 404 page. Minor styling enhancements. Favicon and resume added.

---
+++ 404Error.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<!-- Basic Page Needs
+================================================== -->
+<meta charset="utf-8">
+<title>Mark Uraine</title>
+<meta name="description" content="I'm trying something new out, so don't get excited.">
+<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<!-- Mobile Specific Metas
+================================================== -->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<!-- CSS
+================================================== -->
+<link rel="stylesheet" href="css/master.css">
+<link href='http://fonts.googleapis.com/css?family=Marcellus' rel='stylesheet' type='text/css'>
+
+
+<!-- Favicons
+================================================== -->
+<link rel="shortcut icon" href="images/favicon.ico">
+<!-- Home screen icon  Mathias Bynens http://goo.gl/6nVq0 -->
+<!-- For iPhone 4 with high-resolution Retina display: -->
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/mobile/h/apple-touch-icon.png">
+<!-- For first-generation iPad: -->
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/mobile/m/apple-touch-icon.png">
+<!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
+<link rel="apple-touch-icon-precomposed" href="images/mobile/l/apple-touch-icon-precomposed.png">
+<!-- For nokia devices: -->
+<link rel="shortcut icon" href="images/mobile/l/apple-touch-icon.png">
+
+</head>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7 ]> <body class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <body class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <body class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <body class="no-js ie9"> <![endif]-->
+<!--[if (gte IE 10)|!(IE)]><!--> <body> <!--<![endif]-->
+
+<div id="wrap"> <!-- Only used for absolute positioning the footer on the bottom -->
+
+	<!-- Primary Page Layout
+	================================================== -->
+
+	<section style="margin-top:100px;">
+		<div class="container">
+			<div class="row">
+				<div class="col8">
+					<h6>404 Error: Page Not Found.</h6>
+					<p>Looks like the page you requested has been removed... or maybe you jacked up the URL entry. Either way, <a href="index.html">return to the home page</a>.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+</div><!-- #wrap -->
+
+<!-- JS
+================================================== -->
+<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery.js"><\/script>')</script>
+<script src="js/jquery.scrollTo-1.4.2-min.js"></script><!-- for page scrolling -->
+<script src="js/jquery.localscroll-1.2.7-min.js"></script><!-- for page scrolling -->
+<script src="js/vallenato.js"></script>
+<script src="js/tabs.js"></script>
+<script src="js/tooltip.js"></script>
+<script src="js/jquery.reveal.js"></script>
+<script src="http://use.edgefonts.net/source-sans-pro.js"></script>
+<script type="text/javascript">
+// Tooltip 
+	$(function(){
+		$('[rel=tooltip]').tooltip();
+	});
+</script>
+
+</body>
+</html>
